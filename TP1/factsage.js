01ba3b4f5e0558c8075c4b012907ebfcdc13a86e
efix <soisfrancfrancois@hotmail.com>
2018-01-21 22:03:01 -0500

    Avancement dans la récupération d'information d'un JSON

--- TP1/factsage.js
+++ TP1/factsage.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
-console.log("ddds");
-$('#formule').html("test");
-console.log($('#formule'));
+
+//$("#table").html("lel");
+
+var json = '{ "formule": "Mg +  3 Cu2O +  2 Fe3C = ?"}';
+//var json = loadJSON(data-output.json);
+var obj = JSON.parse(json);
+document.getElementById("table").innerHTML = obj.formule;
+
+/*
+$(document).ready(function(){
+    $("#formule").load("data-output.json");
 });
+*/
+
+/*
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        document.getElementById("table").innerHTML = myObj.formule;
+    }
+};
+xmlhttp.open("GET", "data-output.json", true);
+xmlhttp.send();
+*/
+
+});
+
