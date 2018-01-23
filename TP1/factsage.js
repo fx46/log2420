7ce0fc651ed86fbe9a81d6a9ab8a840684b409d8
François-Xavier Legault <frlegf@l4714-18.info.polymtl.ca>
2018-01-23 11:39:15 -0500

    continuation tableau

--- TP1/factsage.js
+++ TP1/factsage.js
@@ -6,22 +6,27 @@ xmlhttp.onreadystatechange = function() {
       var myObj = JSON.parse(this.responseText);  
      	var noeud = document.getElementById('table');
        
+      var headerRow = document.createElement('tr');
+      noeud.appendChild(headerRow);       // Row pour le header
      	var header = document.createElement('th');
-       
      	header.innerHTML = myObj.formule;   // Va chercher le header
      	noeud.appendChild(header);
+       
+      var emptyHeader = document.createElement('th');
+     	noeud.appendChild(emptyHeader);
       
      	for (var i = 0; i < myObj.output1.length; i++){  
-        noeud.appendChild(document.createElement('tr'));
-        noeud.appendChild(document.createElement('td'));  //on cree une colonne vide
+        var row = document.createElement('tr')
+        noeud.appendChild(row);
+        row.appendChild(document.createElement('td'));  //on cree une colonne vide
          
      		var par = document.createElement('td');
       	par.innerHTML = myObj.output1[i].libelle;
-      	noeud.appendChild(par);
+      	row.appendChild(par);
   	  }
       
       console.log(noeud);
-      noeud.childNodes[3].innerHTML = "test";
+      noeud.childNodes[3].childNodes[0].innerHTML = "test";
       
     }
 };
