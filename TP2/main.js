6a9a419a889c7234c16e9b416810ffaa5c6a538c
François-Xavier Legault <frlegf@l4714-18.info.polymtl.ca>
2018-02-06 09:44:24 -0500

    map fonctionne

--- TP2/main.js
+++ TP2/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
 
-
+var map;
+initMap();
 
 });
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
