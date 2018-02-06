733ae5016d792d32133b536cfdf6edba2f66f0ec
François-Xavier Legault <frlegf@l4714-18.info.polymtl.ca>
2018-02-06 10:18:24 -0500

    Link JSON de bixi

--- TP2/main.js
+++ TP2/main.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
 
-var map;
-initMap();
+  var xmlhttp = new XMLHttpRequest();
+  var map;
+  initMap();
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+    	  var myObj = JSON.parse(this.responseText);  
+        var paragrapheTest = document.getElementById('test');
+        paragrapheTest.innerHTML = "Premiere station dans le JSON: ";
+        paragrapheTest.innerHTML += myObj.stations[0].s;
+        console.log(myObj);
+      }
+  	
+  };
 
+  xmlhttp.open("GET", "https://secure.bixi.com/data/stations.json", true);
+  xmlhttp.send();
 });
 
 function initMap() {
