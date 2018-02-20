67a7649b97b17022d1f0b82160a28106ddd313d2
Efix <soisfrancfrancois@hotmail.com>
2018-02-20 11:24:01 -0500

    Avancement dans autocomplete

--- TP2/main.js
+++ TP2/main.js
@@ -1,30 +1,32 @@
 $(document).ready(function(){
 	
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-    	  var myObj = JSON.parse(this.responseText);  
-        //var paragrapheTest = document.getElementById('test');
-        //paragrapheTest.innerHTML = "Premiere station dans le JSON: ";
-        //paragrapheTest.innerHTML += myObj.stations[0].s;
-        //console.log(myObj);
-      }
-  };
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var myObj = JSON.parse(this.responseText);  
+			var suggests = [myObj.stations.length];
+			for(var i = 0; i < myObj.stations.length; i++){
+				suggests[i] = myObj.stations[i].s;
+			}
+			initAutocomplete(suggests);
+		}
+	};
 
-  xmlhttp.open("GET", "https://secure.bixi.com/data/stations.json", true);
-  xmlhttp.send();
-  
-  var suggests = ["hello", "world", "mots", "test", "sdfsdf"];
-  initAutocomplete(suggests);
-  
-  var map;
-  initMap();
-  
+	xmlhttp.open("GET", "https://secure.bixi.com/data/stations.json", true);
+	xmlhttp.send();
+
+	var map;
+	initMap();
 });
 
 function initAutocomplete(suggests) {
 	$('#inputElem').autocomplete({
 		source : suggests
+		//select : function(event, ui){ // lors de la sélection d'une proposition
+		//	
+		//},
+		//maxShowItems : 5,
+		//minLength : 3
 	});
 }
 
