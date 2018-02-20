d7e9bc607bc6df71296465248473e326efcb5130
Efix <soisfrancfrancois@hotmail.com>
2018-02-20 09:49:58 -0500

    Autocomplete marche!

--- TP2/main.js
+++ TP2/main.js
@@ -1,25 +1,32 @@
 $(document).ready(function(){
-  
-  //var suggests = ["hello", "world"];
-  //$('#inputElem').autocomplete( "option", "delay" );
-
+	
   var xmlhttp = new XMLHttpRequest();
-  var map;
-  initMap();
   xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
     	  var myObj = JSON.parse(this.responseText);  
-        var paragrapheTest = document.getElementById('test');
-        paragrapheTest.innerHTML = "Premiere station dans le JSON: ";
-        paragrapheTest.innerHTML += myObj.stations[0].s;
-        console.log(myObj);
+        //var paragrapheTest = document.getElementById('test');
+        //paragrapheTest.innerHTML = "Premiere station dans le JSON: ";
+        //paragrapheTest.innerHTML += myObj.stations[0].s;
+        //console.log(myObj);
       }
-  	
   };
 
   xmlhttp.open("GET", "https://secure.bixi.com/data/stations.json", true);
   xmlhttp.send();
+  
+  var suggests = ["hello", "world", "mots", "test", "sdfsdf"];
+  initAutocomplete(suggests);
+  
+  var map;
+  initMap();
+  
 });
+
+function initAutocomplete(suggests) {
+	$('#inputElem').autocomplete({
+		source : suggests
+	});
+}
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
