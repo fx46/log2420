e9103314b734f9bfa2e851a7d7058e8f294cd172
Efix <soisfrancfrancois@hotmail.com>
2018-03-13 09:32:46 -0400

    Fichiers de base

---
+++ TP3/main.js
@@ -0,0 +1,7 @@
+/*********************************************************************
+ *  Quand le document est prêt, cette fonction est active.
+ *  Elle appelle toutes les autres fonction pour charger la page.
+ *********************************************************************/
+$(document).ready(function(){
+	
+})
