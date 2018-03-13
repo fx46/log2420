5cbfe97df61f73b79316e3a85cf5184013d59ec1
Efix <soisfrancfrancois@hotmail.com>
2018-03-13 12:01:36 -0400

    Capable d'envoyer un message!

--- TP3/main.js
+++ TP3/main.js
@@ -3,16 +3,21 @@
  *  Elle appelle toutes les autres fonction pour charger la page.
  *********************************************************************/
 $(document).ready(function(){
-	var socketClient = new WebSocket("ws://log2420-nginx.info.polymtl.ca/chatservice?username=Jacob est ma pute");
+	var socketClient = new WebSocket("ws://log2420-nginx.info.polymtl.ca/chatservice?username=FX");
+	
+	var channel;
 	
 	socketClient.onmessage = function(event){
-		console.log(event);
-		console.log(event.data);
+		console.log(JSON.parse(event.data).data);
+		channel = JSON.parse(event.data).data[0].id;
 	}
 
 	socketClient.onopen = function(e){
 		console.log(e)
 	}
 	
+	setTimeout(()=>{
+		var message = new Message("onMessage",channel,"henlo",null,null);
+		socketClient.send(JSON.stringify(message));
+	}, 500);
 })
-
