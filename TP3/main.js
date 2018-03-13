290cf53475d00dc79fca99d36e7bf677c3b6f9e8
Efix <soisfrancfrancois@hotmail.com>
2018-03-13 10:02:39 -0400

    Début

--- TP3/main.js
+++ TP3/main.js
@@ -3,5 +3,16 @@
  *  Elle appelle toutes les autres fonction pour charger la page.
  *********************************************************************/
 $(document).ready(function(){
+	var socketClient = new WebSocket("ws://log2420-nginx.info.polymtl.ca/chatservice?username=Jacob est ma pute");
+	
+	socketClient.onmessage = function(event){
+		console.log(event);
+		console.log(event.data);
+	}
+
+	socketClient.onopen = function(e){
+		console.log(e)
+	}
 	
 })
+
