b7cddd6e71d56a3d81d58d6c15eb09c289e994ee
fx46 <31199346+fx46@users.noreply.github.com>
2018-03-18 09:42:58 -0400

    On peut voir les messages à l'écran et en envoyer!

--- TP3/main.js
+++ TP3/main.js
@@ -1,23 +1,32 @@
+var channel;
+var socketClient;
+
 /*********************************************************************
  *  Quand le document est prêt, cette fonction est active.
  *  Elle appelle toutes les autres fonction pour charger la page.
  *********************************************************************/
 $(document).ready(function(){
-	var socketClient = new WebSocket("ws://log2420-nginx.info.polymtl.ca/chatservice?username=FX");
-	
-	var channel;
+	document.getElementById("test").innerHTML = "Connecting...";
+	socketClient = new WebSocket("ws://log2420-nginx.info.polymtl.ca/chatservice?username=FX");
 	
 	socketClient.onmessage = function(event){
-		console.log(JSON.parse(event.data).data);
-		channel = JSON.parse(event.data).data[0].id;
-	}
-
-	socketClient.onopen = function(e){
-		console.log(e)
-	}
-	
-	setTimeout(()=>{
-		var message = new Message("onMessage",channel,"henlo",null,null);
-		socketClient.send(JSON.stringify(message));
-	}, 500);
+		receiveMessage(event);
+	}	
 })
+
+function sendMessage(){
+	var messageBox = document.getElementById('message');
+	var message = new Message("onMessage", channel, messageBox.value, null, null);
+	socketClient.send(JSON.stringify(message));
+}
+
+function receiveMessage(event){
+	console.log(JSON.parse(event.data));
+	if (JSON.parse(event.data).eventType == "updateChannelsList"){
+		channel = JSON.parse(event.data).data[0].id;
+		document.getElementById("test").innerHTML = "Connected!";
+	}
+	else{
+		$('#messages').append('<div>' + JSON.parse(event.data).data + '</div>');
+	}
+}
